2dd65e6baa66744963142a3913c78de6ccaf455a
Xiao Luo <xluo@niub.la>
2017-07-10 16:44:36 +0800

    add spring dependency

---
+++ src/main/java/xluo/github/spring/springcrud/helloWorldController.java
@@ -0,0 +1,11 @@
+package xluo.github.spring.springcrud;
+
+/**
+ * Created by xiaoluo on 17-7-10.
+ */
+public class helloWorldController {
+
+    public void helloWorld() {
+
+    }
+}
---
+++ src/test/java/xluo/github/spring/springcrud/helloWorldControllerTest.java
@@ -0,0 +1,14 @@
+package xluo.github.spring.springcrud;
+
+import org.junit.Test;
+
+/**
+ * Created by xiaoluo on 17-7-7.
+ */
+public class helloWorldControllerTest {
+
+    @Test
+    public void testHelloWorld() {
+        System.out.println("Hello World!");
+    }
+}
